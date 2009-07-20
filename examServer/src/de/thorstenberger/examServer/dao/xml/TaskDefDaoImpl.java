48fe97891d6dd39bb2e52c6649be4d532a50557f
sdienst <sdienst@49e4f31f-544f-0410-8404-dcdff4eb5335>
2009-07-20 10:12:50 +0000

    [FIX] Store the property "showSolutionToStudents" manually, it's not a property of the jaxb type TaskDefType.
    git-svn-id: https://elateportal.svn.sourceforge.net/svnroot/elateportal/trunk@1090 49e4f31f-544f-0410-8404-dcdff4eb5335

--- examServer/src/de/thorstenberger/examServer/dao/xml/TaskDefDaoImpl.java
+++ examServer/src/de/thorstenberger/examServer/dao/xml/TaskDefDaoImpl.java
@@ -15,7 +15,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+ */
 /**
  *
  */
@@ -31,7 +31,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
 import javax.xml.bind.JAXBContext;
@@ -53,200 +52,203 @@ import de.thorstenberger.examServer.model.TaskDefVO;
 import de.thorstenberger.examServer.service.ExamServerManager;
 
 /**
- *
+ * 
  * TODO add mutator methods and synchronize
- *
+ * 
  * @author Thorsten Berger
- *
+ * 
  */
 public class TaskDefDaoImpl implements TaskDefDao {
 
-	private ExamServerManager examServerManager;
-	private JAXBContext jc;
-	private TaskDefs taskDefs;
-	private File taskDefsFile;
-	private AtomicLong crntId;
+    private final ExamServerManager examServerManager;
+    private JAXBContext jc;
+    private TaskDefs taskDefs;
+    private File taskDefsFile;
+    private AtomicLong crntId;
 
-	private Log log = LogFactory.getLog( TaskDefDaoImpl.class );
+    private final Log log = LogFactory.getLog(TaskDefDaoImpl.class);
 
-	/**
+    /**
 	 *
 	 */
-	public TaskDefDaoImpl( ExamServerManager examServerManager ) {
+    public TaskDefDaoImpl(final ExamServerManager examServerManager) {
 
-		this.examServerManager = examServerManager;
+        this.examServerManager = examServerManager;
 
-		try { // JAXBException
+        try { // JAXBException
 
-			jc = JAXBContext.newInstance( "de.thorstenberger.examServer.dao.xml.jaxb" );
+            jc = JAXBContext.newInstance("de.thorstenberger.examServer.dao.xml.jaxb");
 
-			taskDefsFile = new File( examServerManager.getRepositoryFile().getAbsolutePath() + File.separatorChar + ExamServerManager.SYSTEM + File.separatorChar + "taskdefs.xml" );
+            taskDefsFile = new File(examServerManager.getRepositoryFile().getAbsolutePath() + File.separatorChar
+                    + ExamServerManager.SYSTEM + File.separatorChar + "taskdefs.xml");
 
-			if( !taskDefsFile.exists() ){
-				ObjectFactory oF = new ObjectFactory();
-				taskDefs = oF.createTaskDefs();
-				this.crntId=new AtomicLong(0);
-				save();
-				return;
-			}
+            if (!taskDefsFile.exists()) {
+                final ObjectFactory oF = new ObjectFactory();
+                taskDefs = oF.createTaskDefs();
+                this.crntId = new AtomicLong(0);
+                save();
+                return;
+            }
 
+            // wenn vorhanden, dann auslesen
+            Unmarshaller unmarshaller;
+            unmarshaller = jc.createUnmarshaller();
+            unmarshaller.setValidating(true);
+            final BufferedInputStream bis = new BufferedInputStream(new FileInputStream(taskDefsFile));
+            taskDefs = (TaskDefs) unmarshaller.unmarshal(bis);
+            this.crntId = new AtomicLong(findMostRecentId(taskDefs));
 
-			// wenn vorhanden, dann auslesen
-			Unmarshaller unmarshaller;
-			unmarshaller = jc.createUnmarshaller();
-			unmarshaller.setValidating( true );
-			BufferedInputStream bis = new BufferedInputStream( new FileInputStream( taskDefsFile ) );
-			taskDefs = (TaskDefs) unmarshaller.unmarshal( bis );
-			this.crntId=new AtomicLong(findMostRecentId(taskDefs));
+        } catch (final JAXBException e) {
+            throw new RuntimeException(e);
+        } catch (final IOException e1) {
+            throw new RuntimeException(e1);
+        }
 
+    }
 
-		} catch (JAXBException e) {
-			throw new RuntimeException( e );
-		}catch (IOException e1){
-			throw new RuntimeException( e1 );
-		}
+    private long findMostRecentId(final TaskDefs tds) {
+        long max = -1;
+        final Iterator it = tds.getTaskDef().iterator();
+        while (it.hasNext()) {
+            final TaskDefType taskDef = (TaskDefType) it.next();
+            max = Math.max(max, taskDef.getId());
+        }
+        return max;
+    }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see de.thorstenberger.examServer.tasks.TaskDefDao#getTaskDef(long)
+     */
+    public TaskDefVO getTaskDef(final long id) {
 
-	}
+        final Iterator it = taskDefs.getTaskDef().iterator();
+        while (it.hasNext()) {
+            final TaskDefType taskDef = (TaskDefType) it.next();
 
-	private long findMostRecentId( TaskDefs tds ) {
-		long max = -1;
-		Iterator it = tds.getTaskDef().iterator();
-		while( it.hasNext() ){
-			TaskDefType taskDef = (TaskDefType)it.next();
-			max=Math.max( max, taskDef.getId() );
-		}
-		return max;
-	}
+            if (taskDef.getId() == id) {
 
-	private void save(){
-		try {
+                final TaskDefVO taskDefVO = new TaskDefVO();
+                taskDefVO.setType("complex");
+                taskDefVO.setTitle(taskDef.getTitle());
+                taskDefVO.setShortDescription(taskDef.getShortDescription());
+                taskDefVO.setStopped(taskDef.isStopped());
+                taskDefVO.setId(taskDef.getId());
+                taskDefVO.setDeadline(taskDef.getDeadline() == 0 ? null : taskDef.getDeadline());
+                taskDefVO.setVisible(taskDef.isVisible());
+                if (taskDef.isSetFollowingTaskId()) {
+                    taskDefVO.setFollowingTaskId(taskDef.getFollowingTaskId());
+                }
 
-			Marshaller marshaller = jc.createMarshaller();
-			Validator validator = jc.createValidator();
-			validator.validate( taskDefs );
-			marshaller.setProperty( Marshaller.JAXB_FORMATTED_OUTPUT, new Boolean(true) );
-			BufferedOutputStream bos = new BufferedOutputStream( new FileOutputStream( this.taskDefsFile ) );
-			marshaller.marshal( taskDefs, bos );
+                taskDefVO.setShowSolutionToStudents(taskDef.getComplexTaskDef().isShowSolutionToStudents());
+                taskDefVO.setComplexTaskFile(taskDef.getComplexTaskDef().getComplexTaskFile());
 
-			bos.close();
+                return taskDefVO;
 
-		} catch (JAXBException e) {
-			throw new RuntimeException( e );
-		} catch( IOException e1 ){
-			throw new RuntimeException( e1 );
-		}
+            }
 
-	}
+        }
 
+        return null;
 
-	/* (non-Javadoc)
-	 * @see de.thorstenberger.examServer.tasks.TaskDefDao#getTaskDefs()
-	 */
-	public List<TaskDefVO> getTaskDefs() {
+    }
 
-		List<TaskDefVO> ret = new ArrayList<TaskDefVO>();
+    /*
+     * (non-Javadoc)
+     * 
+     * @see de.thorstenberger.examServer.tasks.TaskDefDao#getTaskDefs()
+     */
+    public List<TaskDefVO> getTaskDefs() {
 
-		Iterator it = taskDefs.getTaskDef().iterator();
-		while( it.hasNext() ){
-			TaskDefType taskDef = (TaskDefType)it.next();
+        final List<TaskDefVO> ret = new ArrayList<TaskDefVO>();
 
-			TaskDefVO taskDefVO = new TaskDefVO();
-			taskDefVO.setType( "complex" );
-			taskDefVO.setTitle( taskDef.getTitle() );
-			taskDefVO.setShortDescription( taskDef.getShortDescription() );
-			taskDefVO.setStopped( taskDef.isStopped() );
-			taskDefVO.setId( taskDef.getId() );
-			taskDefVO.setDeadline( taskDef.getDeadline() == 0 ? null : taskDef.getDeadline() );
-			taskDefVO.setVisible( taskDef.isVisible() );
-			if( taskDef.isSetFollowingTaskId() )
-				taskDefVO.setFollowingTaskId( taskDef.getFollowingTaskId() );
+        final Iterator it = taskDefs.getTaskDef().iterator();
+        while (it.hasNext()) {
+            final TaskDefType taskDef = (TaskDefType) it.next();
 
-			taskDefVO.setShowSolutionToStudents( taskDef.getComplexTaskDef().isShowSolutionToStudents() );
-			taskDefVO.setComplexTaskFile( taskDef.getComplexTaskDef().getComplexTaskFile() );
+            final TaskDefVO taskDefVO = new TaskDefVO();
+            taskDefVO.setType("complex");
+            taskDefVO.setTitle(taskDef.getTitle());
+            taskDefVO.setShortDescription(taskDef.getShortDescription());
+            taskDefVO.setStopped(taskDef.isStopped());
+            taskDefVO.setId(taskDef.getId());
+            taskDefVO.setDeadline(taskDef.getDeadline() == 0 ? null : taskDef.getDeadline());
+            taskDefVO.setVisible(taskDef.isVisible());
+            if (taskDef.isSetFollowingTaskId()) {
+                taskDefVO.setFollowingTaskId(taskDef.getFollowingTaskId());
+            }
 
-			ret.add( taskDefVO );
+            taskDefVO.setShowSolutionToStudents(taskDef.getComplexTaskDef().isShowSolutionToStudents());
+            taskDefVO.setComplexTaskFile(taskDef.getComplexTaskDef().getComplexTaskFile());
 
-		}
-		return ret;
+            ret.add(taskDefVO);
 
-	}
+        }
+        return ret;
 
-	/* (non-Javadoc)
-	 * @see de.thorstenberger.examServer.tasks.TaskDefDao#getTaskDef(long)
-	 */
-	public TaskDefVO getTaskDef( long id ) {
+    }
 
-		Iterator it = taskDefs.getTaskDef().iterator();
-		while( it.hasNext() ){
-			TaskDefType taskDef = (TaskDefType)it.next();
+    private void save() {
+        try {
 
-			if( taskDef.getId() == id ){
+            final Marshaller marshaller = jc.createMarshaller();
+            final Validator validator = jc.createValidator();
+            validator.validate(taskDefs);
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, new Boolean(true));
+            final BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(this.taskDefsFile));
+            marshaller.marshal(taskDefs, bos);
 
-				TaskDefVO taskDefVO = new TaskDefVO();
-				taskDefVO.setType( "complex" );
-				taskDefVO.setTitle( taskDef.getTitle() );
-				taskDefVO.setShortDescription( taskDef.getShortDescription() );
-				taskDefVO.setStopped( taskDef.isStopped() );
-				taskDefVO.setId( taskDef.getId() );
-				taskDefVO.setDeadline( taskDef.getDeadline() == 0 ? null : taskDef.getDeadline() );
-				taskDefVO.setVisible( taskDef.isVisible() );
-				if( taskDef.isSetFollowingTaskId() )
-					taskDefVO.setFollowingTaskId( taskDef.getFollowingTaskId() );
+            bos.close();
 
-				taskDefVO.setShowSolutionToStudents( taskDef.getComplexTaskDef().isShowSolutionToStudents() );
-				taskDefVO.setComplexTaskFile( taskDef.getComplexTaskDef().getComplexTaskFile() );
+        } catch (final JAXBException e) {
+            throw new RuntimeException(e);
+        } catch (final IOException e1) {
+            throw new RuntimeException(e1);
+        }
 
-				return taskDefVO;
+    }
 
-			}
+    public TaskDefVO storeTaskDef(final TaskDefVO td) {
+        if (td.getId() >= 0) {
+            // update
+            final Iterator it = taskDefs.getTaskDef().iterator();
+            while (it.hasNext()) {
+                final TaskDefType taskDef = (TaskDefType) it.next();
+                if (taskDef.getId() == td.getId()) {
+                    try {
+                        BeanUtils.copyProperties(taskDef, td);
+                        taskDef.getComplexTaskDef().setShowSolutionToStudents(td.isShowSolutionToStudents());
+                    } catch (final Exception e) {
+                        log.error("Exception on BeanUtils.copyProperties().", e);
+                    }
+                    break;
+                }
+            }
+        } else {
+            // set new id
+            td.setId(this.crntId.incrementAndGet());
+            try {
+                final ObjectFactory oF = new ObjectFactory();
 
+                final TaskDefType tdt = oF.createTaskDefsTypeTaskDefType();
+                BeanUtils.copyProperties(tdt, td);
 
-		}
+                final ComplexTaskDefType ctdt = oF.createTaskDefsTypeTaskDefTypeComplexTaskDefType();
+                ctdt.setComplexTaskFile(td.getComplexTaskFile());
+                ctdt.setShowSolutionToStudents(td.isShowSolutionToStudents());
+                tdt.setComplexTaskDef(ctdt);
 
-		return null;
-
-	}
-
-	public TaskDefVO storeTaskDef( TaskDefVO td ) {
-		if(td.getId() >=0 ) {
-			//update
-			Iterator it = taskDefs.getTaskDef().iterator();
-			while( it.hasNext() ){
-				TaskDefType taskDef = (TaskDefType)it.next();
-				if(taskDef.getId()==td.getId()) {
-					try{
-						BeanUtils.copyProperties( taskDef, td );
-					}catch( Exception e ){
-						log.error( "Exception on BeanUtils.copyProperties().", e );
-					}
-					break;
-				}
-			}
-		}else {
-			//set new id
-			td.setId( this.crntId.incrementAndGet() );
-			try{
-				ObjectFactory oF = new ObjectFactory();
-
-				TaskDefType tdt = oF.createTaskDefsTypeTaskDefType();
-				BeanUtils.copyProperties( tdt, td );
-
-				ComplexTaskDefType ctdt = oF.createTaskDefsTypeTaskDefTypeComplexTaskDefType();
-				ctdt.setComplexTaskFile( td.getComplexTaskFile() );
-				ctdt.setShowSolutionToStudents( td.isShowSolutionToStudents() );
-				tdt.setComplexTaskDef( ctdt );
-
-				taskDefs.getTaskDef().add( tdt );
-			}catch( JAXBException e ){
-				throw new RuntimeException(e);
-			}catch( IllegalAccessException e ){
-				throw new RuntimeException(e);
-			}catch( InvocationTargetException e ){
-				throw new RuntimeException(e);
-			}
-		}
-		save();
-		return td;
-	}
+                taskDefs.getTaskDef().add(tdt);
+            } catch (final JAXBException e) {
+                throw new RuntimeException(e);
+            } catch (final IllegalAccessException e) {
+                throw new RuntimeException(e);
+            } catch (final InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        save();
+        return td;
+    }
 }

6a5db95fc525bca013f8ef2ad3fe3a8f33984af5
sdienst <sdienst@49e4f31f-544f-0410-8404-dcdff4eb5335>
2009-11-11 14:00:42 +0000

    [fix] increased lock timeout for massively parallel file transaction test, needed to allow our build system to keep up :(
    git-svn-id: https://elateportal.svn.sourceforge.net/svnroot/elateportal/trunk@1161 49e4f31f-544f-0410-8404-dcdff4eb5335

--- examServer/src/test/java/examserver/dao/transaction/FileTransactionTest.java
+++ examServer/src/test/java/examserver/dao/transaction/FileTransactionTest.java
@@ -30,6 +30,7 @@ public class FileTransactionTest {
 	    super(workingDirectory);
     }
 		public void append(byte b) {
+			getFRM().setDefaultTransactionTimeout(10000);
 			String txId = startTransaction();
 			try {
 	      OutputStream os = getFRM().writeResource(txId, "file.txt",true);
@@ -43,6 +44,11 @@ public class FileTransactionTest {
 	      e.printStackTrace();
 	      Assert.fail(e.getMessage());
       }
+		}
+		public void write(String fileresource,String contents) throws IOException, ResourceManagerException {
+			String txId = startTransaction();
+			IOUtils.write(contents,getFRM().writeResource(txId, fileresource));
+			commitTransaction(txId);
 		}
 		public void appendWOTransaction(byte b) {
 			try {
@@ -60,14 +66,15 @@ public class FileTransactionTest {
 		
 	}
 	private FileContentIncrementer io;
+	private File tempDir;
 	
 	@Before
 	public void setup() throws IOException {
-		File tempFile = File.createTempFile("transaction", "test");
-		tempFile.delete();
-		tempFile.mkdir();
+		tempDir = File.createTempFile("transaction", "test");
+		tempDir.delete();
+		tempDir.mkdir();
 		
-		io = new FileContentIncrementer(tempFile.getAbsolutePath());
+		io = new FileContentIncrementer(tempDir.getAbsolutePath());
 	}
 	
 	@Test
@@ -88,5 +95,14 @@ public class FileTransactionTest {
 		// then
 		assertEquals(1000,IOUtils.toByteArray(io.readFile()).length);
 	}
+	@Test
+  public void shouldCreateSubdirsInTransactions() throws Exception {
+	  // given
+	  String subdir = "foo/bar/test.txt";
+	  // when
+	  io.write(subdir, "test");
+	  // then
+	  Assert.assertTrue(new File(tempDir,subdir).exists());
+  }
 
 }

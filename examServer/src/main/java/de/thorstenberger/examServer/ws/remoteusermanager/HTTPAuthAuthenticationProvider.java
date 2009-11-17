6575e1f3393ae7507bf835ab6cbc6c08737a0d62
sdienst <sdienst@49e4f31f-544f-0410-8404-dcdff4eb5335>
2009-11-17 15:13:37 +0000

    [build] generate jaxb sources on every build instead of adding them to the svn. [internal] fixed junit setup
    git-svn-id: https://elateportal.svn.sourceforge.net/svnroot/elateportal/trunk@1168 49e4f31f-544f-0410-8404-dcdff4eb5335

--- examServer/src/main/java/de/thorstenberger/examServer/ws/remoteusermanager/HTTPAuthAuthenticationProvider.java
+++ examServer/src/main/java/de/thorstenberger/examServer/ws/remoteusermanager/HTTPAuthAuthenticationProvider.java
@@ -18,6 +18,8 @@ import org.apache.commons.httpclient.UsernamePasswordCredentials;
 import org.apache.commons.httpclient.auth.AuthScope;
 import org.apache.commons.httpclient.cookie.CookiePolicy;
 import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.context.MessageSource;
 import org.springframework.context.MessageSourceAware;
 import org.springframework.context.support.MessageSourceAccessor;
@@ -32,196 +34,228 @@ import de.thorstenberger.examServer.util.StringUtil;
 import de.thorstenberger.examServer.ws.remoteusermanager.client.UserBean;
 
 public class HTTPAuthAuthenticationProvider extends AbstractUserDetailsAuthenticationProvider implements AuthenticationProvider, MessageSourceAware {
+    private final Log log = LogFactory.getLog(HTTPAuthAuthenticationProvider.class);
 
-	private MessageSourceAccessor messageSourceAccessor;
-	private ConfigManager configManager;
-	private UserManager userManager;
-	private RoleManager roleManager;
-	
-	private boolean forcePrincipalAsString = false;
-	
-	/**
-	 * 
-	 */
-	public HTTPAuthAuthenticationProvider( ConfigManager configManager, UserManager userManager, RoleManager roleManager ) {
-		this.configManager = configManager;
-		this.userManager = userManager;
-		this.roleManager = roleManager;
-	}
-	
-	/* (non-Javadoc)
-	 * @see org.acegisecurity.providers.AuthenticationProvider#authenticate(org.acegisecurity.Authentication)
-	 */
-	public Authentication authenticate(Authentication authentication)
-			throws AuthenticationException {
+    private MessageSourceAccessor messageSourceAccessor;
+    private final ConfigManager configManager;
+    private final UserManager userManager;
+    private final RoleManager roleManager;
 
-		Assert.isInstanceOf(UsernamePasswordAuthenticationToken.class, authentication,
-	            messageSourceAccessor.getMessage("AbstractUserDetailsAuthenticationProvider.onlySupports",
-	                "Only UsernamePasswordAuthenticationToken is supported"));
+    private boolean forcePrincipalAsString = false;
 
-		if( !configManager.isStudentsLoginEnabled() )
-			throw new AuthenticationServiceException( "Login disabled for student role." );
+    /**
+     * 
+     */
+    public HTTPAuthAuthenticationProvider( final ConfigManager configManager, final UserManager userManager, final RoleManager roleManager ) {
+        this.configManager = configManager;
+        this.userManager = userManager;
+        this.roleManager = roleManager;
+    }
 
-		if( configManager.getHTTPAuthURL() != null && configManager.getHTTPAuthURL().length() > 0 ){
-		
-	        // Determine username
-	        String username = (authentication.getPrincipal() == null) ? "NONE_PROVIDED" : authentication.getName();
-	        String password = (authentication.getCredentials() == null) ? "NONE_PROVIDED" : (String)authentication.getCredentials();
-			
-	        UserBean userBean;
+    /* (non-Javadoc)
+     * @see org.acegisecurity.providers.AuthenticationProvider#authenticate(org.acegisecurity.Authentication)
+     */
+    @Override
+    public Authentication authenticate(final Authentication authentication)
+    throws AuthenticationException {
+
+        Assert.isInstanceOf(UsernamePasswordAuthenticationToken.class, authentication,
+                messageSourceAccessor.getMessage("AbstractUserDetailsAuthenticationProvider.onlySupports",
+                "Only UsernamePasswordAuthenticationToken is supported"));
+
+        if( !configManager.isStudentsLoginEnabled() ) {
+            throw new AuthenticationServiceException( "Login disabled for student role." );
+        }
+
+        if( configManager.getHTTPAuthURL() != null && configManager.getHTTPAuthURL().length() > 0 ){
+
+            // Determine username
+            final String username = (authentication.getPrincipal() == null) ? "NONE_PROVIDED" : authentication.getName();
+            final String password = (authentication.getCredentials() == null) ? "NONE_PROVIDED" : (String)authentication.getCredentials();
+
+            UserBean userBean;
             try {
-            	
-            	// verify url
-            	URL url = null;
-            	try {
-					url = new URL(configManager.getHTTPAuthURL());
-				} catch (MalformedURLException e1) {
-					throw new AuthenticationServiceException( "URL-Error for HTTP-Auth." );
-				}
-            	
-				userBean = getRemoteUserInfos( url, username, password );
-				
-				if( !userBean.getRole().equals( "student" ) )
-					throw new AuthenticationServiceException( "Only student role allowed." );
-				
-				
-				try {
-					
-					userManager.getUserByUsername( userBean.getLogin() );
-					
-				} catch( UsernameNotFoundException e ){
-					User user = new User();
-					user.setEnabled( true );
-					user.setUsername( userBean.getLogin() );
-					user.setFirstName( userBean.getFirstName() == null ? "" : userBean.getFirstName() );
-					user.setLastName( userBean.getName() == null ? "" : userBean.getName() );
-					user.setEmail( userBean.getEmail() == null ? "" : userBean.getEmail() );
-					user.setPassword( StringUtil.encodePassword( userBean.getPassword(), "SHA" ) );
-					user.addRole( roleManager.getRole( "student" ) );
-					try {
-						userManager.saveUser( user );
-					} catch ( UserExistsException e2 ) {
-						// should not happen
-						throw new RuntimeException( e2 );
-					}
-				}
-			} catch ( AuthenticationServiceException e ) {
-				throw new BadCredentialsException(messages.getMessage(
+
+                // verify url
+                URL url = null;
+                try {
+                    url = new URL(configManager.getHTTPAuthURL());
+                } catch (final MalformedURLException e1) {
+                    throw new AuthenticationServiceException( "URL-Error for HTTP-Auth." );
+                }
+
+                userBean = getRemoteUserInfos( url, username, password );
+
+                if( !userBean.getRole().equals( "student" ) ) {
+                    throw new AuthenticationServiceException( "Only student role allowed." );
+                }
+
+
+                try {
+
+                    userManager.getUserByUsername( userBean.getLogin() );
+
+                } catch( final UsernameNotFoundException e ){
+                    final User user = new User();
+                    user.setEnabled( true );
+                    user.setUsername( userBean.getLogin() );
+                    user.setFirstName( userBean.getFirstName() == null ? "" : userBean.getFirstName() );
+                    user.setLastName( userBean.getName() == null ? "" : userBean.getName() );
+                    user.setEmail( userBean.getEmail() == null ? "" : userBean.getEmail() );
+                    user.setPassword( StringUtil.encodePassword( userBean.getPassword(), "SHA" ) );
+                    user.addRole( roleManager.getRole( "student" ) );
+                    try {
+                        userManager.saveUser( user );
+                    } catch ( final UserExistsException e2 ) {
+                        // should not happen
+                        throw new RuntimeException( e2 );
+                    }
+                }
+            } catch ( final AuthenticationServiceException e ) {
+                throw new BadCredentialsException(messages.getMessage(
                         "AbstractUserDetailsAuthenticationProvider.badCredentials", "Invalid username or password."));
-			}
-			
-			UserDetails userDetails = userManager.getUserByUsername( username );
-	        
-	        Object principalToReturn = userDetails;
+            }
 
-	        if ( forcePrincipalAsString ) {
-	            principalToReturn = userDetails.getUsername();
-	        }
+            final UserDetails userDetails = userManager.getUserByUsername( username );
 
-	        return createSuccessAuthentication( principalToReturn, authentication, userDetails );
-			
-		}
-		
-		return null;
-		
-	}
+            Object principalToReturn = userDetails;
 
-	private UserBean getRemoteUserInfos( URL url, String login, String pwd ) throws AuthenticationServiceException{
+            if ( forcePrincipalAsString ) {
+                principalToReturn = userDetails.getUsername();
+            }
+
+            return createSuccessAuthentication( principalToReturn, authentication, userDetails );
+
+        }
+
+        return null;
+
+    }
+
+    private UserBean getRemoteUserInfos( final URL url, final String login, final String pwd ) throws AuthenticationServiceException{
         try {
-        	boolean auth = authenticateUser( url, login, pwd );
-        	if ( auth ) {
-        		String email = null;
-        		if ( configManager.getHTTPAuthMail() != null && configManager.getHTTPAuthMail().length() > 0 ) {
-        			email = login + '@' + configManager.getHTTPAuthMail();
-        		}
-        		// now we know that at least login and password are correct, so we create the user bean
-        		UserBean bean = new UserBean( email, login, null, pwd, "student", null );
-        		return bean;
-        	}
-        	throw new AuthenticationServiceException( "" );
-		} catch ( IOException e ) {
-			throw new AuthenticationServiceException( e.getMessage(), e );
-		}
-	}
-	
-	private boolean authenticateUser( URL url, String login, String pwd ) throws IOException {
-		HttpClient client = new HttpClient();
-		client.getParams().setCookiePolicy( CookiePolicy.RFC_2109 );
+            final boolean auth = authenticateUser( url, login, pwd );
+            if ( auth ) {
+                String email = null;
+                if ( configManager.getHTTPAuthMail() != null && configManager.getHTTPAuthMail().length() > 0 ) {
+                    email = login + '@' + configManager.getHTTPAuthMail();
+                }
+                // now we know that at least login and password are correct, so we create the user bean
+                final UserBean bean = new UserBean( email, login, null, pwd, "student", null );
+                return bean;
+            }
+            throw new AuthenticationServiceException( "" );
+        } catch ( final IOException e ) {
+            throw new AuthenticationServiceException( e.getMessage(), e );
+        }
+    }
+
+    private boolean authenticateUser( final URL url, final String login, final String pwd ) throws IOException {
+        final HttpClient client = new HttpClient();
+        client.getParams().setCookiePolicy( CookiePolicy.RFC_2109 );
         client.getState().setCredentials(
-            new AuthScope( url.getHost(), AuthScope.ANY_PORT, AuthScope.ANY_REALM ),
-            new UsernamePasswordCredentials( login, pwd )
+                new AuthScope( url.getHost(), AuthScope.ANY_PORT, AuthScope.ANY_REALM ),
+                new UsernamePasswordCredentials( login, pwd )
         );
-        
-        GetMethod authget = new GetMethod( url.toString() );
+
+        final GetMethod authget = new GetMethod( url.toString() );
 
         authget.setDoAuthentication(true);
-        
+
         String response = null;
         try {
-            int status = client.executeMethod( authget );
-            
+            final int status = client.executeMethod( authget );
+
             // check if successful
             if ( status == 200 )
             {
                 response = authget.getResponseBodyAsString();
 
-                if ( response != null ) {
-                	return true;
-                }
+                return isValidAuthentication(extractSAMLResponse(response));
             }
         } finally {
             // release any connection resources used by the method
-        	authget.releaseConnection();
+            authget.releaseConnection();
         }
-        
+
         return false;
-	}
-	
-	/* (non-Javadoc)
-	 * @see org.acegisecurity.providers.AuthenticationProvider#supports(java.lang.Class)
-	 */
-	public boolean supports(Class authentication) {
-		return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
-	}
+    }
 
-	/* (non-Javadoc)
-	 * @see org.springframework.context.MessageSourceAware#setMessageSource(org.springframework.context.MessageSource)
-	 */
-	public void setMessageSource(MessageSource messageSource) {
-		this.messageSourceAccessor = new MessageSourceAccessor( messageSource );
-	}
+    /**
+     * Verify the signed SAMLResponse xml file using the signature within.
+     * 
+     * @param samlXml
+     *            SAMLResponse
+     * @return true if this authentication is valid
+     */
+    private boolean isValidAuthentication(final String samlXml) {
+        // TODO Auto-generated method stub
+        // log.warn("Invalid IdP authentication: "+samlXml);
+        return false;
+    }
 
-	/* (non-Javadoc)
-	 * @see org.acegisecurity.providers.dao.AbstractUserDetailsAuthenticationProvider#additionalAuthenticationChecks(org.acegisecurity.userdetails.UserDetails, org.acegisecurity.providers.UsernamePasswordAuthenticationToken)
-	 */
-	@Override
-	protected void additionalAuthenticationChecks(UserDetails arg0, UsernamePasswordAuthenticationToken arg1) throws AuthenticationException {
-		// TODO Auto-generated method stub
-		
-	}
+    /**
+     * @param xml
+     * @return
+     */
+    private String extractSAMLResponse(final String html) {
+        if (html == null) {
+            throw new AuthenticationServiceException("Invalid authentication response from the shibboleth IdP!");
+        } else {
 
-	/* (non-Javadoc)
-	 * @see org.acegisecurity.providers.dao.AbstractUserDetailsAuthenticationProvider#retrieveUser(java.lang.String, org.acegisecurity.providers.UsernamePasswordAuthenticationToken)
-	 */
-	@Override
-	protected UserDetails retrieveUser(String arg0, UsernamePasswordAuthenticationToken arg1) throws AuthenticationException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+            return null;
+        }
+    }
 
-	/**
-	 * @return Returns the forcePrincipalAsString.
-	 */
-	public boolean isForcePrincipalAsString() {
-		return forcePrincipalAsString;
-	}
+    /* (non-Javadoc)
+     * @see org.acegisecurity.providers.AuthenticationProvider#supports(java.lang.Class)
+     */
+    @Override
+    public boolean supports(final Class authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
 
-	/**
-	 * @param forcePrincipalAsString The forcePrincipalAsString to set.
-	 */
-	public void setForcePrincipalAsString(boolean forcePrincipalAsString) {
-		this.forcePrincipalAsString = forcePrincipalAsString;
-	}
+    /* (non-Javadoc)
+     * @see org.springframework.context.MessageSourceAware#setMessageSource(org.springframework.context.MessageSource)
+     */
+    @Override
+    public void setMessageSource(final MessageSource messageSource) {
+        this.messageSourceAccessor = new MessageSourceAccessor( messageSource );
+    }
+
+    /* (non-Javadoc)
+     * @see org.acegisecurity.providers.dao.AbstractUserDetailsAuthenticationProvider#additionalAuthenticationChecks(org.acegisecurity.userdetails.UserDetails, org.acegisecurity.providers.UsernamePasswordAuthenticationToken)
+     */
+    @Override
+    protected void additionalAuthenticationChecks(final UserDetails arg0, final UsernamePasswordAuthenticationToken arg1) throws AuthenticationException {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see org.acegisecurity.providers.dao.AbstractUserDetailsAuthenticationProvider#retrieveUser(java.lang.String, org.acegisecurity.providers.UsernamePasswordAuthenticationToken)
+     */
+    @Override
+    protected UserDetails retrieveUser(final String arg0, final UsernamePasswordAuthenticationToken arg1) throws AuthenticationException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * @return Returns the forcePrincipalAsString.
+     */
+    @Override
+    public boolean isForcePrincipalAsString() {
+        return forcePrincipalAsString;
+    }
+
+    /**
+     * @param forcePrincipalAsString The forcePrincipalAsString to set.
+     */
+    @Override
+    public void setForcePrincipalAsString(final boolean forcePrincipalAsString) {
+        this.forcePrincipalAsString = forcePrincipalAsString;
+    }
 
 }
 
--- examServer/src/test/java/examserver/dao/transaction/FileTransactionTest.java
+++ examServer/src/test/java/examserver/dao/transaction/FileTransactionTest.java
@@ -1,7 +1,5 @@
 package examserver.dao.transaction;
 
-import static org.junit.Assert.assertEquals;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -24,85 +22,86 @@ import de.thorstenberger.examServer.dao.AbstractTransactionalFileIO;
  *
  */
 public class FileTransactionTest {
-	class FileContentIncrementer extends AbstractTransactionalFileIO{
+    class FileContentIncrementer extends AbstractTransactionalFileIO{
 
-		public FileContentIncrementer(String workingDirectory) {
-	    super(workingDirectory);
+        public FileContentIncrementer(final String workingDirectory) {
+            super(workingDirectory);
+        }
+        public void append(final byte b) {
+            getFRM().setDefaultTransactionTimeout(30000);
+            final String txId = startTransaction();
+            try {
+                final OutputStream os = getFRM().writeResource(txId, "file.txt",true);
+                os.write(b);
+                os.close();
+                getFRM().commitTransaction(txId);
+            } catch (final ResourceManagerException e) {
+                e.printStackTrace();
+                Assert.fail(e.getMessage());
+            } catch (final IOException e) {
+                e.printStackTrace();
+                Assert.fail(e.getMessage());
+            }
+        }
+        public void write(final String fileresource,final String contents) throws IOException, ResourceManagerException {
+            final String txId = startTransaction();
+            IOUtils.write(contents,getFRM().writeResource(txId, fileresource));
+            commitTransaction(txId);
+        }
+        public void appendWOTransaction(final byte b) {
+            try {
+                final FileOutputStream fos = new FileOutputStream(new File(this.workingPath,"file.txt"),true);
+                fos.write(b);
+                fos.close();
+            }catch(final IOException e) {
+                e.printStackTrace();
+                Assert.fail(e.getMessage());
+            }
+        }
+        public InputStream readFile() throws ResourceManagerException {
+            return getFRM().readResource("file.txt");
+        }
+
     }
-		public void append(byte b) {
-			getFRM().setDefaultTransactionTimeout(30000);
-			String txId = startTransaction();
-			try {
-	      OutputStream os = getFRM().writeResource(txId, "file.txt",true);
-	      os.write(b);
-	      os.close();
-	      getFRM().commitTransaction(txId);
-      } catch (ResourceManagerException e) {
-      	e.printStackTrace();
-	      Assert.fail(e.getMessage());
-      } catch (IOException e) {
-	      e.printStackTrace();
-	      Assert.fail(e.getMessage());
-      }
-		}
-		public void write(String fileresource,String contents) throws IOException, ResourceManagerException {
-			String txId = startTransaction();
-			IOUtils.write(contents,getFRM().writeResource(txId, fileresource));
-			commitTransaction(txId);
-		}
-		public void appendWOTransaction(byte b) {
-			try {
-			FileOutputStream fos = new FileOutputStream(new File(this.workingPath,"file.txt"),true);
-			fos.write(b);
-			fos.close();
-			}catch(IOException e) {
-				e.printStackTrace();
-				Assert.fail(e.getMessage());
-			}
-		}
-		public InputStream readFile() throws ResourceManagerException {
-			return getFRM().readResource("file.txt");
+    private FileContentIncrementer io;
+    private File tempDir;
+
+    @Before
+    public void setup() throws IOException {
+        tempDir = File.createTempFile("transaction", "test");
+        tempDir.delete();
+        tempDir.mkdir();
+
+        io = new FileContentIncrementer(tempDir.getAbsolutePath());
     }
-		
-	}
-	private FileContentIncrementer io;
-	private File tempDir;
-	
-	@Before
-	public void setup() throws IOException {
-		tempDir = File.createTempFile("transaction", "test");
-		tempDir.delete();
-		tempDir.mkdir();
-		
-		io = new FileContentIncrementer(tempDir.getAbsolutePath());
-	}
-	
-	@Test
-	public void shouldNotOverwriteConcurrentFileChanges() throws Exception {
-		// given 10 concurrent threads
-		ExecutorService execService = Executors.newFixedThreadPool(10);
-		Runnable runnable = new Runnable() {
-	    public void run() {
-//	    	io.appendWOTransaction((byte)'x');
-	    	io.append((byte)'x');
-	    }
-    };
-		// when appending a character 100 times
-    for(int i = 0; i< 1000;i++)
-    	execService.submit(runnable);
-    execService.shutdown();
-    execService.awaitTermination(1, TimeUnit.MINUTES);
-		// then
-		assertEquals(1000,IOUtils.toByteArray(io.readFile()).length);
-	}
-	@Test
-  public void shouldCreateSubdirsInTransactions() throws Exception {
-	  // given
-	  String subdir = "foo/bar/test.txt";
-	  // when
-	  io.write(subdir, "test");
-	  // then
-	  Assert.assertTrue(new File(tempDir,subdir).exists());
-  }
+
+    @Test
+    public void shouldNotOverwriteConcurrentFileChanges() throws Exception {
+        // given 10 concurrent threads
+        final ExecutorService execService = Executors.newFixedThreadPool(10);
+        final Runnable runnable = new Runnable() {
+            public void run() {
+                //	    	io.appendWOTransaction((byte)'x');
+                io.append((byte)'x');
+            }
+        };
+        // when appending a character 100 times
+        for(int i = 0; i< 1000;i++) {
+            execService.submit(runnable);
+        }
+        execService.shutdown();
+        execService.awaitTermination(60, TimeUnit.SECONDS);
+        // then
+        Assert.assertEquals(1000, IOUtils.toByteArray(io.readFile()).length);
+    }
+    @Test
+    public void shouldCreateSubdirsInTransactions() throws Exception {
+        // given
+        final String subdir = "foo/bar/test.txt";
+        // when
+        io.write(subdir, "test");
+        // then
+        Assert.assertTrue(new File(tempDir,subdir).exists());
+    }
 
 }

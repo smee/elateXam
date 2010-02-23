3495cf4945a5172b33b3d9cf8515d6d57b32f014
sdienst <sdienst@49e4f31f-544f-0410-8404-dcdff4eb5335>
2010-02-23 09:34:40 +0000

    [build] centralize property expression configuration for builds. Needed because multiple PropertyConfigurer beans are not allowed in spring
    git-svn-id: https://elateportal.svn.sourceforge.net/svnroot/elateportal/trunk@1184 49e4f31f-544f-0410-8404-dcdff4eb5335

--- examServer/src/main/java/de/thorstenberger/examServer/ws/remoteusermanager/FreeForAllAuthenticationProvider.java
+++ examServer/src/main/java/de/thorstenberger/examServer/ws/remoteusermanager/FreeForAllAuthenticationProvider.java
@@ -44,139 +44,147 @@ import de.thorstenberger.examServer.util.StringUtil;
  * 
  */
 public class FreeForAllAuthenticationProvider extends AbstractUserDetailsAuthenticationProvider implements
-        AuthenticationProvider, MessageSourceAware {
+AuthenticationProvider, MessageSourceAware {
 
-    private MessageSourceAccessor messageSourceAccessor;
-    private final ConfigManager configManager;
-    private final UserManager userManager;
-    private final RoleManager roleManager;
+  private MessageSourceAccessor messageSourceAccessor;
+  private final ConfigManager configManager;
+  private final UserManager userManager;
+  private final RoleManager roleManager;
 
-    private boolean forcePrincipalAsString = false;
+  private boolean forcePrincipalAsString = false;
+  private boolean enabled = false;
 
-    /**
-	 * 
-	 */
-    public FreeForAllAuthenticationProvider(final ConfigManager configManager, final UserManager userManager,
-            final RoleManager roleManager) {
-        this.configManager = configManager;
-        this.userManager = userManager;
-        this.roleManager = roleManager;
+  /**
+   * 
+   */
+  public FreeForAllAuthenticationProvider(final ConfigManager configManager, final UserManager userManager,
+      final RoleManager roleManager) {
+    this.configManager = configManager;
+    this.userManager = userManager;
+    this.roleManager = roleManager;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * org.acegisecurity.providers.dao.AbstractUserDetailsAuthenticationProvider#additionalAuthenticationChecks(org.
+   * acegisecurity.userdetails.UserDetails, org.acegisecurity.providers.UsernamePasswordAuthenticationToken)
+   */
+  @Override
+  protected void additionalAuthenticationChecks(final UserDetails arg0, final UsernamePasswordAuthenticationToken arg1) throws AuthenticationException {
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.acegisecurity.providers.AuthenticationProvider#authenticate(org.acegisecurity.Authentication)
+   */
+  @Override
+  public Authentication authenticate(final Authentication authentication)
+  throws AuthenticationException {
+
+    if (!enabled) {
+      return null;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.acegisecurity.providers.dao.AbstractUserDetailsAuthenticationProvider#additionalAuthenticationChecks(org.
-     * acegisecurity.userdetails.UserDetails, org.acegisecurity.providers.UsernamePasswordAuthenticationToken)
-     */
-    @Override
-    protected void additionalAuthenticationChecks(final UserDetails arg0, final UsernamePasswordAuthenticationToken arg1) throws AuthenticationException {
+    Assert.isInstanceOf(UsernamePasswordAuthenticationToken.class, authentication,
+        messageSourceAccessor.getMessage("AbstractUserDetailsAuthenticationProvider.onlySupports",
+        "Only UsernamePasswordAuthenticationToken is supported"));
+
+    if (!configManager.isStudentsLoginEnabled()) {
+      throw new AuthenticationServiceException("Login disabled for student role.");
+    }
+    User user = null;
+    try {
+      user = userManager.getUserByUsername(authentication.getName());
+      // if we know this user, make sure the password matches
+      if (!user.getPassword().equals(StringUtil.encodePassword((String) authentication.getCredentials(), "SHA"))) {
+        throw new BadCredentialsException(messages.getMessage(
+            "AbstractUserDetailsAuthenticationProvider.badCredentials", "Invalid username or password."));
+      }
+    } catch (final UsernameNotFoundException e) {
+      // doesn't matter, we'll create him in a second...
+    }
+    if (user == null) {
+      // create a new user with the entered username/password
+      user = new User();
+      user.setEnabled(true);
+      final String name = authentication.getName();
+      user.setUsername(name);
+      user.setFirstName(Character.toUpperCase(name.charAt(0)) + name.substring(1));
+      user.setLastName("Mustermann");
+      user.setPassword(StringUtil.encodePassword((String) authentication.getCredentials(), "SHA"));
+      user.addRole(roleManager.getRole("student"));
+      try {
+        userManager.saveUser(user);
+      } catch (final UserExistsException e2) {
+        // should not happen
+        throw new RuntimeException(e2);
+      }
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.acegisecurity.providers.AuthenticationProvider#authenticate(org.acegisecurity.Authentication)
-     */
-    @Override
-    public Authentication authenticate(final Authentication authentication)
-            throws AuthenticationException {
+    final UserDetails userDetails = userManager.getUserByUsername(user.getUsername());
 
-        Assert.isInstanceOf(UsernamePasswordAuthenticationToken.class, authentication,
-                messageSourceAccessor.getMessage("AbstractUserDetailsAuthenticationProvider.onlySupports",
-                "Only UsernamePasswordAuthenticationToken is supported"));
+    Object principalToReturn = userDetails;
 
-        if (!configManager.isStudentsLoginEnabled()) {
-            throw new AuthenticationServiceException("Login disabled for student role.");
-        }
-        User user = null;
-        try {
-            user = userManager.getUserByUsername(authentication.getName());
-            // if we know this user, make sure the password matches
-            if (!user.getPassword().equals(StringUtil.encodePassword((String) authentication.getCredentials(), "SHA"))) {
-                throw new BadCredentialsException(messages.getMessage(
-                        "AbstractUserDetailsAuthenticationProvider.badCredentials", "Invalid username or password."));
-            }
-        } catch (final UsernameNotFoundException e) {
-            // doesn't matter, we'll create him in a second...
-        }
-        if (user == null) {
-            // create a new user with the entered username/password
-            user = new User();
-            user.setEnabled(true);
-            final String name = authentication.getName();
-            user.setUsername(name);
-            user.setFirstName(Character.toUpperCase(name.charAt(0)) + name.substring(1));
-            user.setLastName("Mustermann");
-            user.setPassword(StringUtil.encodePassword((String) authentication.getCredentials(), "SHA"));
-            user.addRole(roleManager.getRole("student"));
-            try {
-                userManager.saveUser(user);
-            } catch (final UserExistsException e2) {
-                // should not happen
-                throw new RuntimeException(e2);
-            }
-        }
-
-        final UserDetails userDetails = userManager.getUserByUsername(user.getUsername());
-
-        Object principalToReturn = userDetails;
-
-        if (forcePrincipalAsString) {
-            principalToReturn = userDetails.getUsername();
-        }
-
-        return createSuccessAuthentication(principalToReturn, authentication, userDetails);
-
+    if (forcePrincipalAsString) {
+      principalToReturn = userDetails.getUsername();
     }
 
-    /**
-     * @return Returns the forcePrincipalAsString.
-     */
-    @Override
-    public boolean isForcePrincipalAsString() {
-        return forcePrincipalAsString;
-    }
+    return createSuccessAuthentication(principalToReturn, authentication, userDetails);
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.acegisecurity.providers.dao.AbstractUserDetailsAuthenticationProvider#retrieveUser(java.lang.String,
-     * org.acegisecurity.providers.UsernamePasswordAuthenticationToken)
-     */
-    @Override
-    protected UserDetails retrieveUser(final String arg0, final UsernamePasswordAuthenticationToken arg1) throws AuthenticationException {
-        return null;
-    }
+  }
 
-    /**
-     * @param forcePrincipalAsString
-     *            The forcePrincipalAsString to set.
-     */
-    @Override
-    public void setForcePrincipalAsString(final boolean forcePrincipalAsString) {
-        this.forcePrincipalAsString = forcePrincipalAsString;
-    }
+  public void setEnabled(final boolean value) {
+    this.enabled = value;
+  }
+  /**
+   * @return Returns the forcePrincipalAsString.
+   */
+  @Override
+  public boolean isForcePrincipalAsString() {
+    return forcePrincipalAsString;
+  }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.springframework.context.MessageSourceAware#setMessageSource(org.springframework.context.MessageSource)
-     */
-    @Override
-    public void setMessageSource(final MessageSource messageSource) {
-        this.messageSourceAccessor = new MessageSourceAccessor(messageSource);
-    }
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.acegisecurity.providers.dao.AbstractUserDetailsAuthenticationProvider#retrieveUser(java.lang.String,
+   * org.acegisecurity.providers.UsernamePasswordAuthenticationToken)
+   */
+  @Override
+  protected UserDetails retrieveUser(final String arg0, final UsernamePasswordAuthenticationToken arg1) throws AuthenticationException {
+    return null;
+  }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.acegisecurity.providers.AuthenticationProvider#supports(java.lang.Class)
-     */
-    @Override
-    public boolean supports(final Class authentication) {
-        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
-    }
+  /**
+   * @param forcePrincipalAsString
+   *            The forcePrincipalAsString to set.
+   */
+  @Override
+  public void setForcePrincipalAsString(final boolean forcePrincipalAsString) {
+    this.forcePrincipalAsString = forcePrincipalAsString;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.springframework.context.MessageSourceAware#setMessageSource(org.springframework.context.MessageSource)
+   */
+  @Override
+  public void setMessageSource(final MessageSource messageSource) {
+    this.messageSourceAccessor = new MessageSourceAccessor(messageSource);
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.acegisecurity.providers.AuthenticationProvider#supports(java.lang.Class)
+   */
+  @Override
+  public boolean supports(final Class authentication) {
+    return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+  }
 
 }

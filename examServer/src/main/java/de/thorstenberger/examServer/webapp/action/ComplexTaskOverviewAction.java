d5073d3eae81cf59e553e08356d0e1bb991933ba
Steffen Dienst <sdienst@informatik.uni-leipzig.de>
2010-05-31 14:25:00 +0200

    [fix] include time extension into submission deadline shown when a student tries to resume his try.

--- examServer/src/main/java/de/thorstenberger/examServer/webapp/action/ComplexTaskOverviewAction.java
+++ examServer/src/main/java/de/thorstenberger/examServer/webapp/action/ComplexTaskOverviewAction.java
@@ -17,7 +17,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 /**
- * 
+ *
  */
 package de.thorstenberger.examServer.webapp.action;
 
@@ -47,91 +47,94 @@ import de.thorstenberger.taskmodel.impl.TaskModelViewDelegateObjectImpl;
  */
 public class ComplexTaskOverviewAction extends BaseAction {
 
-	public ActionForward execute(
+	@Override
+  public ActionForward execute(
 	        ActionMapping mapping,
 	        ActionForm form,
 	        HttpServletRequest request,
 	        HttpServletResponse response)
 	        throws Exception {
-		
+
 		long taskId;
-		
+
 		try {
 			taskId = Long.parseLong( request.getParameter( "taskId" ) );
 		} catch (NumberFormatException e) {
 			throw new RuntimeException( e );
 		}
-		
+
 		TaskManager tm = (TaskManager)getBean( "TaskManager" );
 		TaskDef td = tm.getTaskDef( taskId );
 
 		TaskDef_Complex ctd;
-		
+
 		try {
 			ctd = (TaskDef_Complex)td;
 		} catch (ClassCastException e) {
 			throw new RuntimeException( "invalid type: \"" + td.getType() + "\", " + e.getMessage() );
 		}
-		
+
 		ComplexTaskInfoVO tivo = new ComplexTaskInfoVO();
 		tivo.setActive( ctd.isActive() );
-		if( ctd.getDeadline() != null )
-			tivo.setDeadline( DateUtil.getStringFromMillis( ctd.getDeadline() ) );
+		if( ctd.getDeadline() != null ) {
+      tivo.setDeadline( DateUtil.getStringFromMillis( ctd.getDeadline() ) );
+    }
 		tivo.setShortDescription( ctd.getShortDescription() );
 		tivo.setStopped( ctd.isStopped() );
 		tivo.setTitle( ctd.getComplexTaskDefRoot().getTitle() );
 		tivo.setType( ctd.getType() );
 		tivo.setId( "" + ctd.getId() );
-		
+
 		tivo.setMaxTries( ctd.getComplexTaskDefRoot().getTries() );
 		tivo.setTime( ctd.getComplexTaskDefRoot().getTimeInMinutesWithoutKindnessExtensionTime() );
 		tivo.setStartText( ctd.getComplexTaskDefRoot().getStartText() );
 		tivo.setShowHandlingHintsBeforeStart( ctd.getComplexTaskDefRoot().isShowHandlingHintsBeforeStart() );
 		tivo.setSpecificDescription( ctd.getComplexTaskDefRoot().getDescription() );
-		
-		if( request.getUserPrincipal() == null ){
-			throw new RuntimeException( "Not logged in." );
-		}
-		
+
+		if( request.getUserPrincipal() == null )
+      throw new RuntimeException( "Not logged in." );
+
 		String login = request.getUserPrincipal().getName();
 		UserManager userManager = (UserManager)getBean( "userManager" );
 		User user = userManager.getUserByUsername( login );
-		
+
 		ComplexTasklet tasklet = (ComplexTasklet) tm.getTaskletContainer().getTasklet( taskId, login );
-		if( tasklet == null )
-			tasklet = (ComplexTasklet) tm.getTaskletContainer().createTasklet( taskId, login );
-		
+		if( tasklet == null ) {
+      tasklet = (ComplexTasklet) tm.getTaskletContainer().createTasklet( taskId, login );
+    }
+
 		tivo.setUsedTries( tasklet.getComplexTaskHandlingRoot().getNumberOfTries() );
 		tivo.setCanStartNewTry( tasklet.canStartNewTry() );
 		tivo.setCanContinueTry( tasklet.canContinueTry() );
-		
+
 		if( tasklet.canContinueTry() ){
-			if( tasklet.getComplexTaskDefRoot().hasTimeRestriction() )
-				tivo.setCtDeadline( DateUtil.getStringFromMillis(
-						tasklet.getActiveTry().getStartTime() + ctd.getComplexTaskDefRoot().getTimeInMinutesWithoutKindnessExtensionTime() * 60 * 1000 ) );	
+			if( tasklet.getComplexTaskDefRoot().hasTimeRestriction() ) {
+        tivo.setCtDeadline( DateUtil.getStringFromMillis(
+						tasklet.getActiveTry().getStartTime() +tasklet.getActiveTry().getTimeExtension() + ctd.getComplexTaskDefRoot().getTimeInMinutesWithoutKindnessExtensionTime() * 60 * 1000 ) );
+      }
 		}
-		
+
 		if( ctd.isShowCorrectionToUsers() && tasklet.hasOrPassedStatus (Tasklet.Status.SOLVED ) ){
 			tivo.setCorrectionVisible( true);
 		}
-		
-		
-		TaskModelViewDelegateObject delegateObject = new TaskModelViewDelegateObjectImpl( taskId, 
+
+
+		TaskModelViewDelegateObject delegateObject = new TaskModelViewDelegateObjectImpl( taskId,
 				tm,
 				login, user.getFirstName() + " " + user.getLastName(),
 				response.encodeURL( request.getContextPath() + "/TaskViewFactory.html?taskId=" + taskId ) );
-		
-		
-		
+
+
+
 //		request.getSession().setAttribute( EPReservedKeys.PRINCIPAL, request.getUserPrincipal() );
 //		request.getSession().setAttribute( TaskContants.TASK_MODEL_VIEW_DELEGATE_OBJECT_KEY_PREFIX + taskId, delegateObject );
 		String sessionId = request.getSession().getId();
 		TaskModelViewDelegate.storeDelegateObject( sessionId, taskId, delegateObject );
-		
+
 		request.setAttribute( "task", tivo );
-		
+
 		return mapping.findForward( "success" );
-		
+
 
 	}
 
